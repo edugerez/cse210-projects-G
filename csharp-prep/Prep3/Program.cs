66e3e4dd364a08641598a07f092ca02dd52e6ab1
edugerez <selmaredugerez@gmail.com>
2023-01-22 02:48:04 -0300

    push magic number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator= new Random();
+        int magicNumber= randomGenerator.Next(1,100);
+        Console.WriteLine("choose form 1 to 10");      
+        string userInput= Console.ReadLine();
+        int number= int.Parse(userInput);
+
+        while (number!= magicNumber)
+        {
+             if (number>magicNumber)
+                {
+                  Console.WriteLine("try lower");
+                  userInput=Console.ReadLine();
+                }
+                  
+             else
+                {
+                  Console.WriteLine("try higer");
+                  userInput=Console.ReadLine();
+                  
+                }
+        }
+        if (number==magicNumber)
+            {
+                Console.WriteLine("you did it");
+
+            }
+        
     }
 }
