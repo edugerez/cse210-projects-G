761901d505cec7bb818bce4f38e611ac61b4b8a1
edugerez <selmaredugerez@gmail.com>
2023-01-22 00:10:20 -0300

    pushing to github

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,14 @@
 using System;
-
+//Gerez Eduardo edition
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name?");
+        string name= Console.ReadLine();
+        Console.Write("What is your last name?");
+        string lastName= Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {name} {lastName}.");
     }
 }
