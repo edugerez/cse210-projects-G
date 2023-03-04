d5ad8de391145b4e0217dd2079afad1dea03b8bc
edugerez <selmaredugerez@gmail.com>
2023-03-03 21:37:11 -0300

    scrripture progam

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Console.WriteLine("Welcome to the memorizer scripture!");
+
+        List<string> _options = new List<string>();
+        _options.Add("1-List of scripture already memorized");
+        _options.Add("2-practice mermorizing");
+        _options.Add("3-quit");
+       
+        string userOption = "";
+        int optionNumber = 0;
+
+        while (optionNumber !=3){
+            Console.WriteLine();
+            foreach(var option in _options){
+                Console.WriteLine(option);
+            }
+            Console.WriteLine();
+            Console.Write("choose an option to continue");
+            userOption=Console.ReadLine();
+            optionNumber= int.Parse(userOption);
+            if (optionNumber==1){
+                Console.WriteLine("your has choosen option 1");}
+            else if (optionNumber==2){
+                Console.WriteLine("you choosed option 2");}
+            else{
+                Console.WriteLine("you quited");
+            }
+            }
+            }
+    
     }
-}
---
+++ prove/Develop03/scripture.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Scripture
+
+    {
+        private string refeScripture;
+        private string textScripture;
+
+        public void addScripture(){
+            Console.Write("write the scripture reference to be shown");
+            refeScripture= Console.ReadLine();
+
+        }
+}
